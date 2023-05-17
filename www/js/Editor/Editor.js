998c5d25cc421c42213d854e66c6c9b571592583
Tsygankov-Slava <st200399200399@gmail.com>
2023-05-17 11:59:13 +0300

    fixed editor scrollheight generation

--- www/js/Editor/Editor.js
+++ www/js/Editor/Editor.js
@@ -1,4 +1,6 @@
 export default class Editor {
+    #editorScroll;
+
     constructor(mode, matchBrackets, lineNumbers, theme) {
         const config = {
             mode: mode,
@@ -8,6 +10,10 @@ export default class Editor {
         };
 
         this.editor = CodeMirror.fromTextArea(document.getElementById('editor'), config);
+
+        this.#editorScroll = document.getElementsByClassName("CodeMirror-scroll")[0]
+        const editorScrollHeight = document.documentElement.clientHeight - this.#editorScroll.getBoundingClientRect().y;
+        this.#editorScroll.style.height = editorScrollHeight + "px";
     }
 
     setCode(code) {
